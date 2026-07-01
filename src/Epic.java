c96656b1d371efe4711da7d35594860e60a7f267
KhasanAdkhamov <164671138+KhasanAdkhamov@users.noreply.github.com>
2026-07-01 23:43:12 +0300

    связал epic и SubTask

--- src/Epic.java
+++ src/Epic.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Epic extends Task{
+    private List<SubTask> subTaskList = new ArrayList<>();
+
     public Epic(String name, String description) {
         super(name, description);
+    }
+
+    public void addSubtask(SubTask subTask) {
+        subTaskList.add(subTask);
+        subTask.setEpic(this);
+        System.out.println(subTask.getName() + " Добавлена подзадача в эпик");
+    }
+
+    public List<SubTask> getSubTaskList() {
+        return subTaskList;
     }
 }
--- src/SubTask.java
+++ src/SubTask.java
@@ -1,5 +1,15 @@
 public class SubTask extends Task{
+    private Epic epic;
+
     public SubTask(String name, String description) {
         super(name, description);
+    }
+
+    public void setEpic(Epic epic) {
+        this.epic = epic;
+    }
+
+    public Epic getEpic() {
+        return epic;
     }
 }
